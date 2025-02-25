170c7b7124bd6c8833b95eb623dea8602319e4b3
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-25 11:12:05 +0300

    chore: add community page

---
+++ app/community/page.js
@@ -0,0 +1,11 @@
+const CommunityPage = () => {
+    return (
+        <>
+            <main>
+                <h1>community page</h1>
+            </main>
+        </>
+    )
+}
+
+export default CommunityPage
