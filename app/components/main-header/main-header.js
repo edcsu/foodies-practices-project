5e8f85a3d6fd6909cc85cc6d7027a16af3adfe95
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-25 12:56:44 +0300

    style: create nav link component

--- app/components/main-header/main-header.js
+++ app/components/main-header/main-header.js
@@ -1,14 +1,11 @@
-"use client"
-
 import Link from "next/link"
 import logoImg from '@/assets/logo.png'
 
 import classes from './main-header.module.css'
 import Image from "next/image"
-import { usePathname } from "next/navigation"
+import NavLink from "./nav-link"
 
 const MainHeader = () => {
-    const path = usePathname()
     return (
         <>
             <header className={classes.header}>
@@ -24,18 +21,10 @@ const MainHeader = () => {
                 <nav className={classes.nav}>
                     <ul>
                         <li>
-                            <Link 
-                                href="/meals"
-                                className={path.startsWith('/meals') ? classes.active: undefined} 
-                            >
-                                Browse Meals
-                            </Link>
+                            <NavLink href="/meals" children="Browse Meals" />
                         </li>
                         <li>
-                            <Link 
-                                href="/community"
-                                className={path.startsWith('/community') ? classes.active: undefined} 
-                            >Community</Link>
+                            <NavLink href="/community">Community</NavLink>
                         </li>
                     </ul>
                 </nav>
