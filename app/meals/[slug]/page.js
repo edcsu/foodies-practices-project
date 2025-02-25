66ee26e3d459428e8c68fdf1e164cb6da2067670
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-25 16:19:05 +0300

    style: update meal details page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,16 +1,27 @@
 import Link from "next/link"
 
+import classes from './page.module.css'
+import Image from "next/image"
+
 const MealPage = async ({ params}) => {
     const { slug } = await params
     
     return (
         <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1> Meal details</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${'EMAIL'}`}>NAME</a>
+                    </p>
+                    <p className={classes.summary}>SUMMARY</p>
+                </div>
+            </header>
             <main>
-                <h1> Meal details</h1>
-                <p> {slug} </p>
-                <p>
-                    <Link href="/meals">Meals</Link>
-                </p>
+                <p className={classes.instrunctions} dangerouslySetInnerHTML={{ __html: '....'}} > {slug} </p>
             </main>
         </>
     )
