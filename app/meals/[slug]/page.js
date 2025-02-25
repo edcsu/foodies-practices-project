87b82da2139953ed85a41b56628bac99fe597b2d
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-25 20:23:25 +0300

    style: add dynamic metadata

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -5,6 +5,19 @@ import Image from "next/image"
 import { getMeal } from "@/lib/meals"
 import { notFound } from "next/navigation"
 
+export async function generateMetadata({params}) {
+    const meal = getMeal(params.slug)
+
+    if (!meal) {
+        notFound()
+    }
+
+    return {
+        title: meal.title,
+        description: meal.summary
+    }
+}
+
 const MealPage = async ({ params}) => {
     const { slug } = await params
     const meal = await getMeal(slug)
