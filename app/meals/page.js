f5748acbeb0a095752b994219065fca9c04f7634
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-25 11:07:32 +0300

    chore: add meals page

---
+++ app/meals/page.js
@@ -0,0 +1,11 @@
+const MealsPage = () => {
+    return (
+        <>
+            <main>
+                <h1> Meals page</h1>
+            </main>
+        </>
+    )
+}
+
+export default MealsPage
