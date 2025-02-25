91159c748da148119f90026957ec3962f79735ec
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-25 11:09:52 +0300

    chore: add share page

---
+++ app/meals/share/page.js
@@ -0,0 +1,11 @@
+const SharePage = () => {
+    return (
+        <>  
+            <main>
+                <h1>Share Page</h1>
+            </main>
+        </>
+    )
+}
+
+export default SharePage
